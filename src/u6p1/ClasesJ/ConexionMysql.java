9cf282d06036a84d97b64e0b6d48ca0f6a503b3a
reyes <reyes@Desktop-Epic>
2021-12-03 12:35:17 -0600

    Avance 2

--- src/u6p1/ClasesJ/ConexionMysql.java
+++ src/u6p1/ClasesJ/ConexionMysql.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package u6p1.Clases;
+package u6p1.ClasesJ;
 
 import java.sql.*;
 import java.util.logging.Level;
