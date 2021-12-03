c0f18c670a7b7a47efda1d36ee673c832ca44a71
reyes <reyes@Desktop-Epic>
2021-12-03 12:48:01 -0600

    Avance 2

---
+++ src/u6p1/ClasesJ/clase2ejemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package u6p1.ClasesJ;
+
+/**
+ *
+ * @author reyes
+ */
+public class clase2ejemplo {
+
+    public clase2ejemplo() {
+    }
+    
+}
