b2770c424d1a2857a9cd9d2a91b44e4e921a688f
reyes <reyes@Desktop-Epic>
2021-12-03 21:32:15 -0600

    Prueba2 -Se creo una nueva clase

---
+++ src/u6p1/ClasesJ/Claseejemplo3.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package u6p1.ClasesJ;
+
+/**
+ *
+ * @author reyes
+ */
+public class Claseejemplo3 {
+    
+}
